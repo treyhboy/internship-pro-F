216bfe8b7528ab9de9c3b67b8629e3f9a14709cb
Treyhboy <taruntrehen@gmail.com>
2017-12-26 02:54:53 +0530

    starting page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './script';
 import './App.css';
 
 class App extends Component {
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+    return (<div id="header">
+          <div className="text-box">
+          <h1 className="heading-primary">
+          <span className="heading-primary-main">Typing</span>
+          <span className="heading-primary-sub">is where you press keys</span>
+      </h1>
+      <a href="#" className="btn btn-white btn-animated">Login</a>
+          <a href="#" className="btn btn-white btn-animated">SignUp</a>
+          </div>
+          </div>
     );
   }
 }
